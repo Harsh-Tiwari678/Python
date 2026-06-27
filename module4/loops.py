9ea7cc551cb3eef37bef8d50b0849e58a5656a20
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-06-27 14:06:58 -0400

    completed the concepts of module4

---
+++ module4/loops.py
@@ -0,0 +1,18 @@
+# for i in range(3):
+#     print("hii i am learning python")
+
+#working with range 
+# for i in range (10 , -1 ,-1):
+#     print(i)
+
+# for letters in "python":
+#     print(letters)
+
+# count = 0
+# while(count<=5):
+#     print("it's fun to learn python")
+#     count+=1
+
+students = ["harsh", "madhav", "naman"]
+for index , student in enumerate(students):
+    print(index,student)
