0436d9ad245699d490fc284257ef2bae9120f4f9
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-06-27 13:07:54 -0400

    module 3 done

---
+++ module3/conditionals.py
@@ -0,0 +1,12 @@
+ #ternary opertors 
+# age = int(input('ENTER YOUR AGE '))
+# status = "Adult" if age >= 18 else "Minor"
+# print("STATUS: ", status)
+
+
+#assingment 1 
+balance = 15000
+withdraw = int(input(" ENTER THE AMOUNT YOU WANNA WITHDRAW "))
+print()
+finalans = "Amount Withdraw" if withdraw<=balance and withdraw>0 else "Unsufficient Balance"
+print(finalans)
