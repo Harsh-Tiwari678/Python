f7ee7381d618745c22448f6ec45fcdd6d5c05f2a
Harsh Tiwari <harshtiwari1806@gmail.com>
2026-06-27 04:12:22 -0400

    Module 1 done

---
+++ module1/basics.py
@@ -0,0 +1,75 @@
+# x,y,z = 10,20,30
+# print(x)
+# print(y)
+# print(z)
+
+# x= y= z = 10
+# print(x)
+# print(y)
+# print(z)
+
+# age  = 10
+# print(type(age))
+
+
+# is_student  = True
+# age  = 10
+# if(age>=18):
+#     print("yes he/she can vote " + str(is_student))
+# else:
+#     print("no he/she cannot vote" + " " + str(is_student)) 
+
+# age = "21"
+# age = int(age) #the int() converts the string into an integer 
+# print(age+6)
+
+#Assingment 1 
+
+# name = "HARSH"
+# CGPA = 9.5
+# is_HostelStudet = True
+# age = 21
+# cource = "B.Tech"
+# print("Name: " + name )
+# print("CGPA: " + str(CGPA))
+# print("Is Hostel Student: " + str(is_HostelStudet))
+# print("Age: " + str(age))
+# print("Cource: " + str(cource))
+
+#Assingment 2 
+
+# a = 10
+# b = 12
+# addition = a + b
+# substraction = a-b 
+# multiplication = a*b
+# division = a/b
+# floordivison = a//b
+# modulus = a%b
+# exponiaation = a**b
+# print ("Addition: " + str(addition))
+# print ("Substraction: " + str(substraction))
+# print ("Multiplication: " + str(multiplication))
+# print ("Division: " + str(division))
+# print ("Floordivison: " + str(floordivison))
+# print ("Modulus: " + str(modulus))
+# print ("Exponiaation: " + str(exponiaation))
+
+
+#Assingment 3
+
+
+name = str(input("ENTTER YOUR NAME : "))
+age =  int (input("ENTER YOUR AGE "))
+cgpa = float (input("ENTER YOUR CGPA "))
+print()
+if (age>=18 and cgpa>=8.0):
+    scholarship_Eligible  = True
+else:
+    scholarship_Eligible= False
+print("Name : " + str(name))
+print("Age: " + str(age))
+print("CGPA : " + str(cgpa))
+print("Scholarship_Eligible : " + str(scholarship_Eligible))
+
+
